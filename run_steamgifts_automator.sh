1c100502e085edc02b2f7f9afd7e72e41a5037c1
Omer Elbaz <omereelbaz3@gmail.com>
2024-08-17 12:27:47 +0300

    chore: remove unused script file

--- run_steamgifts_automator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-exec >> /tmp/steamgifts_automator.log 2>&1
-echo "Script started at $(date)"
-cd /Users/omerelbaz/Documents/GitHub/steamgifts-automator || { echo "Failed to change directory"; exit 1; }
-/usr/local/bin/bun /Users/omerelbaz/Documents/GitHub/steamgifts-automator/index.ts || echo "Bun command failed with exit code $?"
-echo "Script ended at $(date)"
